1ad71feb367a8a65858e9d58f6f4907df3c342df
tarunagarwal99 <tarunprahladka965@gmail.com>
2021-07-22 11:02:50 +0530

    .

--- 19.cpp
+++ 19.cpp
@@ -31,15 +31,33 @@ vector<int> vec;
 
     //    auto it = find(vec.begin(),vec.end(),1);    //for finding the occurance of 2 in vector
 
-    // binary search  //logn complexityl
+    // binary search  //logn complexityl  //Binary function is a boolean function
     bool ab = binary_search(arr,arr+n,8); 
     bool ab  = binary_search(vec.begin(),vec.end(),8);
 
     //lower bound function
-
+    // binary search is implemented inside the lower_bound function
     //returns an iterator pointing to the first element which is not greater then x
 
     //arr{} = {1,5,7,7,8,10,10,10,11,11,12} 
-    //if x=10 then ans is 8
+    //if x=10 then ans is 10
+  //  if x=7 then ans is 7    7 is the first element which is  not lesser then 7
+  //if x=6 the ans is 7
+//   if x=13 then ans is end 
+    auto it = lower_bound(arr,arr+n,x);
+    int ind =it-arr;
+
+    auto it = lower_bound(vec.begin(),vec.end(),x);    // for vector
+    // int inde = it - vec.begin();
+
+
+    //upper_bound is a fucntion which is just greater then x
+
+   // arr[] = {1,5,7,7,8,10,10,10,11,11,12}
+   //if x=7 then it will pointing to 8
+   //if x =6 then it will pointing to 7
+   //if x = 12 then it will pointing to the end() operator
+
+   auto it = upper_bound(arr,arr+n,x);   //same function implemented as lower_bound
     return 0;
 }
--- 20.cpp
+++ 20.cpp
@@ -1,9 +1,39 @@
-//binary search
+//alogrithm
+//q1- find me the  first index where the element x lies
+
+
+// if we use find function that will gives us o(N) time complexity
+//the array is sorted 
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i<n; i++)
+    {
+        cin>>n;
+    }
+    int x;
+    cin>>x;
+    //method 1
+    int ind = lower_bound(arr,arr+n,x);
+    int ind = lower_bound(arr,arr+n,x)-arr;
+
+
+    //2nd method
+    //if index is nopt inside in your range then it will show us an runtime error 
+    int ind = lower_bound(arr,arr+n,x)-arr;
+    if(ind !=n && arr[ind] == x){
+        cout<<"found at:"<<ind;
+    }
+    else{
+        cout<<"not found";
+    }
+    
     
     return 0;
 }
